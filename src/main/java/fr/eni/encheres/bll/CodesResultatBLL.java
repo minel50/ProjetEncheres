c9a6bbf79ea7280f4cc558b57dd38bdf4774f8be
Grégory Dubois <gregory.dubois@gadz.org>
2021-06-16 15:02:30 +0200

    Création package bll

---
+++ src/main/java/fr/eni/encheres/bll/CodesResultatBLL.java
@@ -0,0 +1,12 @@
+package fr.eni.encheres.bll;
+
+/**
+ * Plages de codes entre 20000 et 29999
+ */
+public class CodesResultatBLL {
+
+	/**
+	 * premier code à utiliser, remplacer cette description et le nom du code ci-dessous
+	 */
+	//public static final int CODE = 20000;
+}
